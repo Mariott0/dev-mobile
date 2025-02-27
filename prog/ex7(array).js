1cd1cf568f08c85f562c3acfc0b8faf5bd23e2ae
Mariott0 <loucosgamer124@gmail.com>
2025-02-26 22:19:42 -0300

    update

---
+++ prog/ex7(array).js
@@ -0,0 +1,6 @@
+let frutas = ["banana", "maçã", "laranja"];
+frutas.forEach((fruta) => console.log(fruta));
+
+let numeros = [1, 2, 3, 4, 5];
+let dobrados = numeros.map((num) => num * 2);
+console.log(dobrados);
